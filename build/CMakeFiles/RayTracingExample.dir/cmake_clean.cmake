f6b799af8231a8eeeab50ee1549bceeb40704d77
sami <sami@pop-os.localdomain>
2024-10-25 12:46:55 +0200

    add to github

---
+++ build/CMakeFiles/RayTracingExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayTracingExample.dir/src/main.cpp.o"
+  "CMakeFiles/RayTracingExample.dir/src/main.cpp.o.d"
+  "bin/RayTracingExample"
+  "bin/RayTracingExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTracingExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
